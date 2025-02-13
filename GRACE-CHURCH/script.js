f74c0c1ced6d7c89ac9abd70951c4aa04c04e301
Ominyi Ajene Jacob <ominyi2k6@gmail.com>
2025-02-12 16:45:46 -0800

    Seekers

---
+++ GRACE-CHURCH/script.js
@@ -0,0 +1,9 @@
+function toggleRotation() {
+    const image = document.getElementById('rotating-image');
+    image.classList.toggle('rotate');
+}
+
+// Optional: Automatically start the rotation when the page loads
+window.onload = function() {
+    toggleRotation();
+};
